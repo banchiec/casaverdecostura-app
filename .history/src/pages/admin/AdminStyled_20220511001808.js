caa3d9553bb4594180ec734529c6e44ae13569fa
banchiec <icaro_19@hotmail.com>
2022-05-15 01:27:54 +0200

    Add Footer and styles in galleryItem

---
+++ .history/src/pages/admin/AdminStyled_20220511001808.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+export const ContainerAdminPage = styled.div`
+	display: flex;
+	flex-direction: row ; 
+	min-height: 100%;
+	.container_screen {
+		width: 80%;
+		background: red;
+		border: 1px solid ; 
+	}
+	.container-shop {
+		display: flex ;
+		position: relative ;
+		padding-bottom: 10em ;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+`
